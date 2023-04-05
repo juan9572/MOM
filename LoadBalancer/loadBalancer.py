8c58c4799d5bd61bc204d416f6c9da3e762069ad
juan9572 <jprinconu@eafit.edu.co>
2023-04-04 23:26:23 -0500

    Toleracia a fallos

--- LoadBalancer/loadBalancer.py
+++ LoadBalancer/loadBalancer.py
@@ -5,41 +5,65 @@ from dotenv import load_dotenv
 from http.server import BaseHTTPRequestHandler
 
 servers = []
+master = 0
 current_server = 0
+tries = 0
+try_servers = 1
 
 def get_server():
     global current_server
     val = (current_server + 1) % len(servers)
-    current_server = val if val != 0 else 1
-    return servers[current_server]
+    current_server = val if val != master else val + 1
+    return (servers[current_server], current_server)
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        server_url = servers[0]
-        self.proxy(server_url)
+        server_url = servers[master]
+        self.proxy(server_url[0], server_url[1])
+        try_servers = 1
 
     def do_DELETE(self):
-        server_url = servers[0]
-        self.proxy(server_url)
+        server_url = servers[master]
+        self.proxy(server_url[0], server_url[1])
+        try_servers = 1
 
     def do_GET(self):
         server_url = get_server()
-        self.proxy(server_url)
+        self.proxy(server_url[0], server_url[1])
+        try_servers = 1
 
-    def proxy(self, server_url):
-        url = f'http://{server_url["ip"]}:{server_url["port"]}{self.path}'
-        resp = requests.request(
-            method=self.command,
-            url=url,
-            data=self.rfile.read(int(self.headers.get('Content-Length', 0)))
-        )
-        self.send_response(resp.status_code)
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        for header in headers:
-            self.send_header(*header)
-        self.end_headers()
-        self.wfile.write(resp.content)
+    def proxy(self, server_url, index):
+        global master
+        global try_servers
+        try:
+            url = f'http://{server_url["ip"]}:{server_url["port"]}{self.path}'
+            resp = requests.request(
+                method=self.command,
+                url=url,
+                data=self.rfile.read(int(self.headers.get('Content-Length', 0)))
+            )
+            self.send_response(resp.status_code)
+            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+            headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+            for header in headers:
+                self.send_header(*header)
+            self.end_headers()
+            self.wfile.write(resp.content)
+        except Exception as e:
+            if tries == 3:
+                if self.command == 'GET':
+                    server_url = get_server()
+                    self.proxy(server_url[0], server_url[1])
+                else:
+                    master = (current_server + 1) % len(servers)
+                    server_url = servers[master]
+                    self.proxy(server_url[0], server_url[1])
+                tries = 0
+                try_servers += 1
+                if try_servers == len(servers):
+                    return
+            tries += 1
+            self.proxy(server_url, index)
 
 if __name__ == '__main__':
     load_dotenv()
